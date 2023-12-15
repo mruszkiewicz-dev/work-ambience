394ac285505d2abe2a3ebb0955a49a9bafc3851d
mruszkiewicz-dev <mruszkiewicz.dev@gmail.com>
2023-12-15 15:11:50 +0100

    style Edit component

--- app/components/Edit.tsx
+++ app/components/Edit.tsx
@@ -3,11 +3,12 @@
 import { useState } from 'react'
 import { EditButton } from './EditButton'
 import { Button, Input } from '@material-tailwind/react'
+import { clsx } from 'clsx'
 
 export const Edit = () => {
   const [active, setActive] = useState(false)
   const [workType, setWorkType] = useState(String)
-  const [time, setTime] = useState([{ work: 0, break: 0 }])
+  const [time, setTime] = useState([{ work: 45, break: 5 }])
 
   const handleTimeChange = (index: number, field: string, value: string) => {
     const newTime: any = [...time]
@@ -19,7 +20,12 @@ export const Edit = () => {
     <>
       {active && (
         <div className='bg-blue-gray-500 w-screen h-screen absolute inset-0 z-30 flex justify-center bg-opacity-70'>
-          <div className='bg-blue-gray-700 p-10 rounded-md z-4 w-200 h-50 p-0 flex flex-col items-center justify-center text-center m-28 p-1'>
+          <div
+            className={clsx(
+              'bg-blue-gray-700 p-10 rounded-md z-40 w-200 h-20 flex flex-col items-center justify-center text-center m-28',
+              workType !== '' ? 'h-80' : 'h-20 ',
+            )}
+          >
             <div className='space-x-2 p-1'>
               <Button
                 className='bg-blue-gray-900'
@@ -35,20 +41,27 @@ export const Edit = () => {
               </Button>
             </div>
             {workType !== '' && (
-              <div className='flex w-72 flex-col items-end gap-6'>
+              <div className='flex w-20 flex-col gap-2 mt-4 text-center'>
+                <label>Work</label>
                 <input
-                  type='number'
                   placeholder='Work'
-                  className='bg-blue-gray-900 text-white'
+                  className='bg-blue-gray-900 text-white rounded-md p-2'
                   value={time[0].work}
                   onChange={(e) => handleTimeChange(0, 'work', e.target.value)}
                 />
+                <label>Break</label>
                 <input
                   placeholder='Break'
-                  className='bg-blue-gray-900 text-white'
+                  className='bg-blue-gray-900 text-white rounded-md p-2'
                   value={time[0].break}
-                  onChange={(e) => handleTimeChange(0, 'work', e.target.value)}
+                  onChange={(e) => handleTimeChange(0, 'break', e.target.value)}
                 />
+                <Button
+                  variant='outlined'
+                  className='bg-blue-gray-100 text-center p-2 mt-4'
+                >
+                  Zapisz
+                </Button>
               </div>
             )}
           </div>
