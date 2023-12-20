33b492d7eb1ea6b384ca31dd65f6e0fdad972cf5
mruszkiewicz-dev <mruszkiewicz.dev@gmail.com>
2023-12-20 15:22:38 +0100

    Steper value

--- app/components/Steper.tsx
+++ app/components/Steper.tsx
@@ -9,10 +9,10 @@ export const Steper = ({ timer }) => {
             <div className='absolute left-0 top-2/4 h-0.5 w-full -translate-y-2/4 bg-gray-300'></div>
             <div className='absolute left-0 top-2/4 h-0.5 w-full -translate-y-2/4 bg-gray-900 transition-all duration-500'></div>
             <div className='relative z-10 grid w-20 h-10 font-bold text-white transition-all duration-300 bg-gray-900 rounded-full place-items-center'>
-              Work
+              Work {item.work}
             </div>
-            <div className='relative z-10 grid w-20 h-10 font-bold text-gray-900 transition-all duration-300 bg-gray-300 rounded-full place-items-center'>
-              Break
+            <div className='relative z-10 grid w-20 h-10 font-bold text-gray-900 transition-all duration-300 bg-gray-600 rounded-full place-items-center'>
+              Break {item.break}
             </div>
           </>
         ))}
